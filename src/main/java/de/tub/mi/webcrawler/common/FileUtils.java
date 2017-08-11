3fed4cc621131dea6ae40a1d562b955f8b4367b3
Muhammad Imran <mimran.646726@gmail.com>
2017-08-11 19:11:32 +0200

    added some functions to FileUtils class

--- src/main/java/de/tub/mi/webcrawler/common/FileUtils.java
+++ src/main/java/de/tub/mi/webcrawler/common/FileUtils.java
@@ -3,52 +3,60 @@ package de.tub.mi.webcrawler.common;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author mi
- *
+ * The FileUtils class contains many functionalities related to a file and directory.
  */
-/**
- * @author mi
- *
- */
-/**
- * @author mi
- *
- */
+
 public class FileUtils {
 	
 	/**
-	 * @param file
+	 * @param path is an absolute path to file to read.
+	 * @return true if the current process has read permission to the file; false if it doesn't have read permission
+	 * @throws NullPointerException
 	 */
-	public static void deleteFile(File file) {
-	
+	public static boolean canReadFile(String path) throws NullPointerException, SecurityException  {
+		File file = new File(path);
+		return file.canRead();
 	}
 	
 	/**
-	 * @param directory
+	 * @param path is an absolute path to file to write.
+	 * @return true if the current process has write permission to the file; false if it doesn't have read permission
+	 * @throws NullPointerException
 	 */
-	public static void deleteDirectory(File directory) {
-		
+	public static boolean canWriteFile(String path) throws NullPointerException, SecurityException  {
+		File file = new File(path);
+		return file.canWrite();
 	}
 	
 	/**
-	 * 
+	 * @param path is an absolute path to file or directory to delete.
+	 * @return true if the current process has write permission to the file; false if it doesn't have read permission
+	 * @throws NullPointerException
 	 */
-	public static void createDirectory() {
-		
+	public static void deleteFileOrDirectory(String path) throws SecurityException, NullPointerException {
+		File file = new File(path);
+		file.delete();
 	}
-	
-	
+
 	/**
-	 * @param parentDirectory is the location of the directory where you want to create a file.
-	 * @param fileName is the name of the file you want to create.
+	 * @param parentDirectory is the location of the directory where you want to create a directory.
 	 * @throws IOException
 	 */
-	public static void createFile(String parentDirectory, String fileName) throws IOException {
-		String path = String.join(parentDirectory, fileName);
+	public static void createDirectory(String path) throws SecurityException, NullPointerException {
+		File directory = new File(path);
+		directory.mkdir();
+	}
+		
+	/**
+	 * @param parentDirectory is the location of the directory where you want to create a file.
+	 * @throws IOException
+	 */
+	public static void createFile(String path) throws SecurityException, IOException {
 		File f = new File(path);
 		f.getParentFile().mkdirs(); 
 		f.createNewFile();
@@ -59,7 +67,7 @@ public class FileUtils {
 	 * @return true if the file is a symbolic link; false if the file does not exist, is not a symbolic link, or it cannot be determined if the file is a symbolic link or not.
 	 * @throws Exception 
 	 */
-	public static boolean isSymbolicLink(Path path) throws Exception {
+	public static boolean isSymbolicLink(Path path) throws SecurityException, NullPointerException, Exception {
 		if (path == null)
 			throw new NullPointerException("The provided path is null.");	
 		return Files.isSymbolicLink(path);
