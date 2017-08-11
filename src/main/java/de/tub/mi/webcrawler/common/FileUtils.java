c325831594c1da87408f64c7ae02ec81128d6c62
Muhammad Imran <mimran.646726@gmail.com>
2017-08-11 17:21:02 +0200

    added some code to FileUtils function

--- src/main/java/de/tub/mi/webcrawler/common/FileUtils.java
+++ src/main/java/de/tub/mi/webcrawler/common/FileUtils.java
@@ -1,5 +1,68 @@
 package de.tub.mi.webcrawler.common;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+
+/**
+ * @author mi
+ *
+ */
+/**
+ * @author mi
+ *
+ */
+/**
+ * @author mi
+ *
+ */
 public class FileUtils {
+	
+	/**
+	 * @param file
+	 */
+	public static void deleteFile(File file) {
+	
+	}
+	
+	/**
+	 * @param directory
+	 */
+	public static void deleteDirectory(File directory) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public static void createDirectory() {
+		
+	}
+	
+	
+	/**
+	 * @param parentDirectory is the location of the directory where you want to create a file.
+	 * @param fileName is the name of the file you want to create.
+	 * @throws IOException
+	 */
+	public static void createFile(String parentDirectory, String fileName) throws IOException {
+		String path = String.join(parentDirectory, fileName);
+		File f = new File(path);
+		f.getParentFile().mkdirs(); 
+		f.createNewFile();
+	}
+	
+	/**
+	 * @param path to the file
+	 * @return true if the file is a symbolic link; false if the file does not exist, is not a symbolic link, or it cannot be determined if the file is a symbolic link or not.
+	 * @throws Exception 
+	 */
+	public static boolean isSymbolicLink(Path path) throws Exception {
+		if (path == null)
+			throw new NullPointerException("The provided path is null.");	
+		return Files.isSymbolicLink(path);
+	}
 
 }
