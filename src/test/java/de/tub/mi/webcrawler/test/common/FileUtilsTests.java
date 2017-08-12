9eebd1042fd2bd7520ab426229af6baa77099b36
Muhammad Imran <mimran.646726@gmail.com>
2017-08-12 13:38:14 +0200

    some changes to the fileutilstest file

--- src/test/java/de/tub/mi/webcrawler/test/common/FileUtilsTests.java
+++ src/test/java/de/tub/mi/webcrawler/test/common/FileUtilsTests.java
@@ -4,7 +4,6 @@
 package de.tub.mi.webcrawler.test.common;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,170 +17,164 @@ import de.tub.mi.webcrawler.common.FileUtils;
 public class FileUtilsTests {
 	
 	@Test
-	public void canReadFileTest(String path) {
+	public void canReadFileTest1() {
 		try {
-			Assert.assertEquals(false, FileUtils.canReadFile(null));
+			Assert.assertEquals(false, FileUtils.canReadFile("~/nonexistent10"));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("blabla"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("/withoutpermissions"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("/realfile"));		
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		
 	}
 	
 	@Test
-	public void canWriteFileTest(String path)  {
+	public void canReadFileTest2() {
 		try {
-			Assert.assertEquals(false, FileUtils.canWriteFile(null));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("blabla"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("/withoutpermissions"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.canReadFile("/realfile"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void deleteFileOrDirectoryTest(String path) {
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory(null));	
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("blabla"));
-
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/withoutpermissions"));
-	
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/realfile"));
-			
-	
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("blabla"));
-	
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/withoutpermissions"));
-			
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/realdir"));
-	
-		} catch (Exception ex) {
-			ex.printStackTrace();			
-		}
-	}
-
-	@Test
-	public void createDirectoryTest(String path) {
-		try {
-			Assert.assertEquals(false, FileUtils.createDirectory(null));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.createDirectory("blabla"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.createDirectory("/withoutpermissions"));
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.createDirectory("/realdir"));
+			Assert.assertEquals(true, FileUtils.canReadFile("/dev/null"));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}		
 	}
-
+	
 	@Test
-	public void createFileTest(String path) {
+	public void canReadFileTest3() {
 		try {
-			Assert.assertEquals(false, FileUtils.createFile(null));
-			
-		} catch(IOException ex) {
+			Assert.assertEquals(true, FileUtils.canReadFile("/home/mi/test200"));		
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void canWriteFileTest2()  {
 		try {
-			Assert.assertEquals(false, FileUtils.createFile("blabla"));
-			
-		} catch(IOException ex) {
+			Assert.assertEquals(true, FileUtils.canWriteFile("/dev/null"));
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void canWriteFileTest3()  {
 		try {
-			Assert.assertEquals(false, FileUtils.createFile("/withoutpermissions"));
-			
-		} catch(IOException ex) {
+			Assert.assertEquals(true, FileUtils.canWriteFile("/home/mi/test100"));
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void deleteFileOrDirectoryTest1() {
 		try {
-			Assert.assertEquals(false, FileUtils.createFile("/realfile"));
-			
+			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("~/nonexistent1"));
+		} catch (Exception ex) {
+			ex.printStackTrace();			
+		}
+	}
+	
+	@Test
+	public void deleteFileOrDirectoryTest3() {
+		try {
+			Assert.assertEquals(true, FileUtils.deleteFileOrDirectory("/home/mi/test"));
+		} catch (Exception ex) {
+			ex.printStackTrace();			
+		}
+	}
+	
+	@Test
+	public void deleteFileOrDirectoryTest4() {
+		try {
+			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/home/mi/nonexistent21"));
+	
+		} catch (Exception ex) {
+			ex.printStackTrace();			
+		}
+	}
+	
+	@Test	
+	public void deleteFileOrDirectoryTest5() {
+		try {
+			Assert.assertEquals(false, FileUtils.deleteFileOrDirectory("/dev/null"));
+		} catch (Exception ex) {
+			ex.printStackTrace();			
+		}
+	}
+	
+	@Test
+	public void deleteFileOrDirectoryTest6() {
+		try {
+			Assert.assertEquals(true, FileUtils.deleteFileOrDirectory("/home/mi/test3"));
+		} catch (Exception ex) {
+			ex.printStackTrace();			
+		}
+	}
+	
+	@Test
+	public void createDirectoryTest1() {
+		try {
+			Assert.assertEquals(true, FileUtils.createDirectory("/home/mi/nonexistent5010"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void createDirectoryTest3() {
+		try {
+			Assert.assertEquals(false, FileUtils.createDirectory("/home/mi/test5"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void createFileTest1() {
+		try {
+			Assert.assertEquals(false, FileUtils.createFile("/home/mi/existing"));
 		} catch(IOException ex) {
 			ex.printStackTrace();
 		}
 	}
 	
 	@Test
-	public void isSymbolicLinkTest(Path path) {
+	public void createFileTest2() {
 		try {
-			Assert.assertEquals(false, FileUtils.isSymbolicLink(null));
+			Assert.assertEquals(false, FileUtils.createFile("/dev/null"));
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void createFileTest3() {
+		try {
+			Assert.assertEquals(true, FileUtils.createFile("/home/mi/newfile101"));
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void isSymbolicLinkTest1() {
+		try {
+			Assert.assertEquals(false, FileUtils.isSymbolicLink("/home/mi/hellofile"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void isSymbolicLinkTest2() {
 		try {
-			Assert.assertEquals(false, FileUtils.isSymbolicLink("blabla"));
+			Assert.assertEquals(false, FileUtils.isSymbolicLink("/dev/null"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void isSymbolicLinkTest3() {
 		try {
-			Assert.assertEquals(false, FileUtils.isSymbolicLink("/withoutpermissions"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
-			Assert.assertEquals(false, FileUtils.isSymbolicLink("/realfile"));
+			Assert.assertEquals(true, FileUtils.isSymbolicLink("/etc/systemd/system/syslog.service"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
